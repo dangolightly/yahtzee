535de71e842e197e8e44c5535ee7d75f3f85fd65
dangolightly <dgol321@hotmail.com>
2026-04-03 19:22:45 -0600

    Refresh installed PWA shell

--- app.js
+++ app.js
@@ -561,7 +561,7 @@ els.scoreboardBody.addEventListener("click", (event) => {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js?v=6").then((registration) => {
+    navigator.serviceWorker.register("./sw.js?v=7").then((registration) => {
       registration.update();
     }).catch(() => {
       // Service worker registration failure does not block gameplay.
--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "yahtzee-cabin-v5";
+const CACHE_NAME = "yahtzee-cabin-v6";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=8",
-  "./app.js?v=8",
+  "./styles.css?v=9",
+  "./app.js?v=9",
   "./manifest.webmanifest",
   "./icon.svg",
 ];

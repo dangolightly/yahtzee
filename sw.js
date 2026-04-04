b51d354d8f5de9d81f7dc290f985bbbd550d0c33
dangolightly <dgol321@hotmail.com>
2026-04-04 07:42:49 -0600

    Label category header version

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "yahtzee-cabin-v18";
+const CACHE_NAME = "yahtzee-cabin-v19";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=21",
-  "./app.js?v=21",
+  "./styles.css?v=22",
+  "./app.js?v=22",
   "./manifest.webmanifest",
   "./icon.svg",
 ];
